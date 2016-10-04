c523626ba9e607e53098119d545595e742e9e832
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-10-04 01:40:35 -0400

    Add license comments.

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,10 +1,27 @@
-#! /usr/bin/env python
-# pylint: disable=unused-import
+#
+# test_suite.py
+#
+# Copyright (c) 2016 Junpei Kawamoto
+#
+# This file is part of rgmining-synthetic-dataset.
+#
+# rgmining-synthetic-dataset is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# rgmining-synthetic-dataset is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
 """Test suite.
 """
 from __future__ import absolute_import
 import importlib
-import itertools
 import sys
 import unittest
 
