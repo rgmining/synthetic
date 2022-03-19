c68ddbe4492ebfed59ecd9be6bb701207530f909
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2022-03-18 22:07:21 -0600

    Rewrite synthetic package with type hints

--- synthetic/__init__.py
+++ synthetic/__init__.py
@@ -1,22 +1,22 @@
 #
-# __init__.py
+#  __init__.py
 #
-# Copyright (c) 2016-2017 Junpei Kawamoto
+#  Copyright (c) 2016-2022 Junpei Kawamoto
 #
-# This file is part of rgmining-synthetic-dataset.
+#  This file is part of rgmining-synthetic-dataset.
 #
-# rgmining-synthetic-dataset is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#  rgmining-synthetic-dataset is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-# rgmining-synthetic-dataset is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#  rgmining-synthetic-dataset is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with rgmining-synthetic-dataset. If not, see <http://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU General Public License
+#  along with rgmining-synthetic-dataset. If not, see <http://www.gnu.org/licenses/>.
 #
 """Provides a method to load a synthetic review dataset.
 
@@ -26,9 +26,9 @@ Applications.
 
 The number of anomalous reviewers this dataset has is defined in
 :data:`ANOMALOUS_REVIEWER_SIZE`, and each anomalous reviewer has `anomaly`
-in theirname.
+in their name.
 
-This package provides a metho `load`, which is an alias of
+This package provides a method `load`, which is an alias of
 :meth:`synthetic.loader.load`. The method takes a graph instance and adds
 reviewers, products, and reviews to the graph.
 
@@ -47,8 +47,13 @@ reviewers, products, and reviews to the graph.
 .. _Masatoshi Yoshikawa: http://www.db.soc.i.kyoto-u.ac.jp/~yoshikawa/
 
 """
-from __future__ import absolute_import
+from typing import Final
+
 from synthetic.loader import load
 
-ANOMALOUS_REVIEWER_SIZE = 57
+ANOMALOUS_REVIEWER_SIZE: Final = 57
 """The number of anomalous reviewers in this synthetic dataset. """
+
+__version__: Final = "0.9.3"
+
+__all__: Final = ("load", "ANOMALOUS_REVIEWER_SIZE", "__version__")
